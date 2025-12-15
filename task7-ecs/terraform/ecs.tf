320df29246704b3a53fb3515789122be3ddaed9f
neerajnakka <neerajnakka8@gmail.com>
2025-12-15 17:31:02 +0530

    Fix: Enable SSL with rejectUnauthorized:false for RDS

--- task7-ecs/terraform/ecs.tf
+++ task7-ecs/terraform/ecs.tf
@@ -101,7 +101,7 @@ resource "aws_ecs_task_definition" "strapi" {
         { name = "API_TOKEN_SALT", value = var.api_token_salt },
         { name = "ADMIN_JWT_SECRET", value = var.admin_jwt_secret },
         { name = "JWT_SECRET", value = var.jwt_secret },
-        { name = "DATABASE_SSL", value = "false" }, # Fix: Disable strict SSL for internal RDS connection
+        { name = "DATABASE_SSL", value = "{\"rejectUnauthorized\": false}" }, # Fix: Enable SSL but skip CA check
       ]
     }
   ])
